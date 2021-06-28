9ebc7a8912073b956849e33e4413229d9ee8e250
gouthami678 <78576477+gouthami678@users.noreply.github.com>
2021-06-28 10:05:47 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>7 DAY LONG FREE TABELU FUNDAMENTALS BOOTCAMP</h1>
+        <p>Certificates...Hands on project... No prerequisites.....</p>
+    </div>
+  );
+}
+export default Info;
---
+++ Info2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info2() {
+  return (
+    <div className="note">
+      <h1>7 DAY LONG FREE PYTHON AND NATURAL LANGUAGE PROCESSING BOOTCAMP</h1>
+        <p>Certificates...Hands on project... No prerequisites.....</p>
+    </div>
+  );
+}
+export default Info2;
